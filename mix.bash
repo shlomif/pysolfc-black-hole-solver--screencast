15fa5b4533d1b418baeba4c65ee787af6521df30
Shlomi Fish <shlomif@shlomifish.org>
2018-04-28 15:30:15 +0300

    Add the successful simplescreenrecorder try.

--- mix.bash
+++ mix.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
-ofn=pysolfc-bhs-with-audio.ogv
+ofn=pysolfc-bhs-with-audio.mkv
 rm -f "$ofn"
-ffmpeg -i "006 - David Amber - Gnarly _feat. Devyn Rush_.ogg" -i pysolfc-bhs.ogv -map 1:v -map 0:a -shortest -c:a libvorbis -q:a 7 -c:v copy -af asetpts=PTS-STARTPTS "$ofn"
+foo="-af asetpts=PTS-STARTPTS"
+foo=""
+aud="006 - David Amber - Gnarly _feat. Devyn Rush_.ogg"
+aud="gnarly.wav"
+ffmpeg -i "$aud" -i pysolfc-bhs.mkv -map 1:v -map 0:a -shortest -c:a libvorbis -q:a 7 -c:v copy $foo  "$ofn"
 # ffmpeg -i audiodump.wav -i pysolfc-bhs.ogv -map 1:v -map 0:a -shortest -c:a libvorbis -q:a 7 -c:v copy -af asetpts=PTS-STARTPTS pysolfc-bhs-with-audio.ogv
 
